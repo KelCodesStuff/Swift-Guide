b5a4bf6e0b989c03b57f41329cddafd1cea55460
Kelvin Reid <kelvin.reid@homes.com>
2017-09-18 16:48:03 -0400

    Updated to Swift 4.0 and added comments

--- Arrays.playground/Contents.swift
+++ Arrays.playground/Contents.swift
@@ -1,27 +1,44 @@
-//: Playground - noun: a place where people can play
+// last checked with Xcode 9.0 GM
+#if swift(>=4.0)
+print("Hello, Swift 4!")
+#endif
 
 import UIKit
 
-// Arrays
+// Arrays //
 
-var magicSpells = ["fire",  "water", "lightning", "ice", "earth", "wind", "light"]
+// create array named magicSpells
+var magicSpells = ["Fire",  "Water", "Lightning", "Ice", "Earth", "Wind", "Light"]
 
+// counts the number of elements in the array which is 7
 print(magicSpells.count)
 
+// displays the 3rd element in the array which is Ice
 print(magicSpells[3])
 
-magicSpells.append("dark")
+// adds Dark to the end of the array
+magicSpells.append("Dark")
 
+// counts the number of elements in the array after Dark is added which is 8
 print(magicSpells.count)
 
-magicSpells.removeAtIndex(6)
+// removes the 6th element in the array which is Light
+magicSpells.remove(at:6)
+
+// removes the 1st element in the array which is Fire
 magicSpells.removeFirst()
+
+// counts the number of elements in the array after Light and Fire are removed, array is now 8
 print(magicSpells.count)
 
-magicSpells.append("light")
-magicSpells.append("fire")
+// adds Teleport to the end of the array
+magicSpells.append("Teleport")
+
+// adds Invisible to the end of the array
+magicSpells.append("Invisible")
+
+// counts the number of elements in the array after Teleport and  is added which is 8
 print(magicSpells.count)
 
+// sorts the array in alphabetical order
 magicSpells.sort()
-
-print(magicSpells.count)
