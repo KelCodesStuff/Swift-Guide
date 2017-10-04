d56696a49b6bcef1b1f500af20069b5896e9e673
KelCodesStuff <codesmithk@gmail.com>
2017-10-03 22:49:08 -0400

    Adding enum examples

--- Enumerations/Enumerations.playground/Contents.swift
+++ Enumerations/Enumerations.playground/Contents.swift
@@ -2,4 +2,46 @@
 
 import UIKit
 
-var str = "Hello, playground"
+// Defining Enums
+
+enum Food {
+    case pizza
+    case tacos
+    case steak
+    case rice
+}
+
+enum NbaTeams {
+    case Lakers, Warriors, Cavaliers, Spurs
+}
+
+// Defining Enums with raw values
+
+enum GameCompanies: String {
+    case Sony = "PlayStation"
+    case Microsoft = "Xbox"
+    case Nintendo = "Switch"
+}
+
+var message = "I hope Sony releases the \(GameCompanies.Sony.rawValue) 5 soon!"
+
+// Enums with switch statements
+
+enum VirginiaParks {
+    case Trashmore, Lakewood, LifeValley
+}
+
+var destination = VirginiaParks.Trashmore
+var warning = ""
+
+switch destination {
+case .Trashmore:
+    warning = "Watch out for the trash"
+case .Lakewood:
+    warning = "Don't fall in the lake"
+case .LifeValley:
+    warning = "Don't get lost in the valley"
+default:
+    warning = "Where are you going?"
+}
+
